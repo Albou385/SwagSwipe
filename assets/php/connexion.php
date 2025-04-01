00a9b78a665221fded222b555a0a48202c5e5f7c
VincentM123 <vincent.martin417@gmail.com>
2025-03-31 22:06:33 -0400

    ajout d'un utilisateur a partir du site web

--- assets/php/connexion.php
+++ assets/php/connexion.php
@@ -1,23 +1,23 @@
 
 
 <?php
+// Configuration de la base de données
+$host = 'localhost'; // Nom d'hôte de la base de données (par exemple, 'localhost')
+$dbname = 'SwagSwipe'; // Nom de la base de données
+$username = 'root'; // Nom d'utilisateur de la base de données
+$password = ''; // Mot de passe de la base de données
 
-//Connexion a la base de donnee
-$host = "localhost"; // Adresse du serveur (127.0.0.1 en local)
-$dbname = "swagswipe"; // Nom de la base de données
-$username = "root"; // Nom d'utilisateur MySQL
-$password = ""; // Mot de passe MySQL
-
+// Tentative de connexion
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    // Configuration pour afficher les erreurs en mode développement
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connexion réussie !<br>";
+    echo "Connexion réussie à la base de données !<br> Httdocs";
+
 
 } catch (PDOException $e) {
-    die("Erreur de connexion : " . $e->getMessage());
+    echo "Erreur de connexion à la base de données : " . $e->getMessage();
     echo "Test";
+    exit();
 }
-
-
-
 ?>
